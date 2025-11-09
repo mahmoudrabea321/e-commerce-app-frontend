aa0006a6241112f0fcac82fcc54d1fc18fffcc9f
Mahmoud Rabea Assaf <bodamomo2010@gmail.com>
2025-11-09 16:07:57 +0200

    rg

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API = import.meta.env.VITE_API_URL || "https://e-commerce-app-backend-1-85px.onrender.com/";
+export const API = import.meta.env.VITE_API_URL || "https://e-commerce-app-backend-1-85px.onrender.com";
